c2e6a5697a19ee4383d01bcbbd95e561b6e1d9f7
Parker Rosenbauer <parkerrosenbauer@gmail.com>
2025-03-18 20:34:38 -0500

    updated endpoint

--- src/chaos-seeds/chaos-seeds.controller.ts
+++ src/chaos-seeds/chaos-seeds.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Post } from '@nestjs/common';
 import { ChaosSeedsService } from './chaos-seeds.service';
 import { ChaosSeedCreationDto } from './dtos/chaos-seed-creation';
 
-@Controller('chaos-seeds')
+@Controller('chaos-seed')
 export class ChaosSeedsController {
   constructor(private readonly chaosSeedsService: ChaosSeedsService) {}
 
