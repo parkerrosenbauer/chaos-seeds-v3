6248a063f20b265a3b94cb95e2c3e2921a8053aa
Parker Rosenbauer <parkerrosenbauer@gmail.com>
2025-03-19 20:59:03 -0500

    separated the db into its own module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ChaosSeedsModule } from './chaos-seeds/chaos-seeds.module';
 import { AreasModule } from './areas/areas.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [ChaosSeedsModule, AreasModule],
+  imports: [ChaosSeedsModule, AreasModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/database/database.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: process.env.NODE_ENV === 'test' ? '.env.test' : '.env',
+    }),
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        dialect: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        autoLoadModels: true,
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  exports: [SequelizeModule],
+})
+export class DatabaseModule {}
