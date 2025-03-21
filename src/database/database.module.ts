f058fb7d6e5a7c1a6223c7035b3e83e4b54e0ed7
Parker Rosenbauer <parkerrosenbauer@gmail.com>
2025-03-20 21:04:05 -0500

    updated config settings

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,27 +1,39 @@
-import { Module } from '@nestjs/common';
+import { Module, OnApplicationShutdown } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { SequelizeModule } from '@nestjs/sequelize';
+import { SequelizeModule, SequelizeModuleOptions } from '@nestjs/sequelize';
+import { Sequelize } from 'sequelize-typescript';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
+      isGlobal: true,
       envFilePath: process.env.NODE_ENV === 'test' ? '.env.test' : '.env',
     }),
     SequelizeModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        dialect: 'postgres',
-        host: configService.get('DB_HOST'),
-        port: configService.get('DB_PORT'),
-        username: configService.get('DB_USER'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_NAME'),
-        autoLoadModels: true,
-        synchronize: true,
-      }),
       inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        const config: SequelizeModuleOptions = {
+          dialect: 'postgres',
+          host: configService.get('DB_HOST'),
+          port: Number(configService.get('DB_PORT')),
+          username: configService.get('DB_USER'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_NAME'),
+          autoLoadModels: true,
+          synchronize: true,
+        };
+        return config;
+      },
     }),
   ],
   exports: [SequelizeModule],
 })
-export class DatabaseModule {}
+export class DatabaseModule implements OnApplicationShutdown {
+  constructor(private readonly sequelize: Sequelize) {}
+
+  onApplicationShutdown() {
+    console.log('Closing database connection');
+    this.sequelize.close();
+  }
+}
