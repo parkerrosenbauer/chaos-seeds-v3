e5c7886b55a6d797d1e099f72a29bfed4a0c1be6
Parker Rosenbauer <parkerrosenbauer@gmail.com>
2025-03-22 20:29:14 -0500

    updated imports of additional models

--- src/chaos-seeds/chaos-seeds.module.ts
+++ src/chaos-seeds/chaos-seeds.module.ts
@@ -5,11 +5,28 @@ import { AreasModule } from '../areas/areas.module';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { ChaosSeed } from './models/chaos-seed';
 import { Area, Biome, Region } from '../areas/models';
+import {
+  Ability,
+  ChaosSeedAbility,
+  ChaosSeedLanguage,
+  Language,
+  Race,
+} from './models';
 
 @Module({
   imports: [
     AreasModule,
-    SequelizeModule.forFeature([ChaosSeed, Area, Region, Biome]),
+    SequelizeModule.forFeature([
+      ChaosSeed,
+      Race,
+      Language,
+      Ability,
+      ChaosSeedAbility,
+      ChaosSeedLanguage,
+      Area,
+      Region,
+      Biome,
+    ]),
   ],
   controllers: [ChaosSeedsController],
   providers: [ChaosSeedsService],
